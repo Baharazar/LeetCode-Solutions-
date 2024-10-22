91052b4d7c768ca72db51541e0c560238c97d4cd
Bahar Nikfalazar <122558394+Baharazar@users.noreply.github.com>
2024-10-22 09:47:17 -0400

    Time: 0 ms (100.00%) | Memory: 11.6 MB (93.07%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,27 +1,29 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        while s != '':
-            if '()' in s:
-                x = s.split('()')
-                s = ''
-                for item in x:
-                    s = s + item
-            elif '[]' in s:
-                x = s.split('[]')
-                s = ''
-                for item in x:
-                    s = s + item
-            elif ('{'+'}') in s:
-                x = s.split(('{'+'}'))
-                s = ''
-                for item in x:
-                    s = s + item
-            else: 
-                return False
-                break
+        # Define the mapping of open to close brackets
+        open_brackets = ['(', '{', '[']
+        close_brackets = [')', '}', ']']
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        stack = []
         
-        return True
+        # First, check if the length of the string is odd, if so return False
+        if len(s) % 2 != 0:
+            return False
+        
+        # Traverse the string
+        for char in s:
+            if char in open_brackets:
+                # If it's an opening bracket, push it onto the stack
+                stack.append(char)
+            elif char in close_brackets:
+                # If it's a closing bracket, check if the stack is empty
+                if not stack:
+                    return False
+                # Pop the last opening bracket from the stack
+                last_open_bracket = stack.pop()
+                # Check if the last opening bracket matches the current closing bracket
+                if bracket_map[char] != last_open_bracket:
+                    return False
+        
+        # At the end, the stack should be empty if all brackets matched
+        return not stack
